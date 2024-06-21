21ba64e641620159d7146df8032c9474dbd71e20
suwatbch <suwat.bch@gmail.com>
2024-06-21 09:26:10 +0700

    update no check

--- settings/database.php
+++ settings/database.php
@@ -45,6 +45,7 @@ return array (
     'leave_cota' => 'leave_cota',
     'leave_items' => 'leave_items',
     'logs' => 'logs',
+    'shift' => 'shift',
     'user' => 'user',
     'user_meta' => 'user_meta',
   ),
